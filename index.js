124cf2399b8023a7c859bb50d65144d26b2e9d59
Shivam <shivamgarg260@gmail.com>
2019-11-09 22:24:59 +0530

    added user route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 require("./modals/dbconnection");
+const User = require("./routes/User");
+
+app.use(express.json());
+app.use(User);
 app.get("/", (req, res) => {
   console.log("incoming req");
   res.send("Hello World");
 });
 
 app.listen(8080, (err, cb) => {
-  console.log("App Running On 3000");
+  console.log("App Running On 8080");
 });
---
+++ routes/User.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const User = require("../modals/User");
+const auth = require("../middleware/auth");
+const router = express.Router();
+
+//Register a User
+router.post("/user/register", async (req, res) => {
+  // console.log("imcoming req");
+  try {
+    let user = await User.findOne({
+      username: req.body.username
+    });
+    if (!user) {
+      let myarray = new Array(31);
+      for (i = 0; i < 31; i++) {
+        myarray[i] = new Array(5);
+        for (let j = 0; j < 5; j++) {
+          myarray[i][j] = false;
+        }
+      }
+      req.body.messcard = myarray;
+      user = new User(req.body);
+
+      await user.save();
+      const token = await user.generateAuthToken();
+      res.status(201).send({
+        user,
+        token
+      });
+    } else {
+      //409=already exists
+      res.status(409).send({ error: 5 });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({ e });
+  }
+});
+
+//Login a user
+router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({
+      user,
+      token
+    });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/user/me", auth, (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
