ea1176d0642ccbd9ef856c90ca99b0b3b8aa4680
Shivam <shivamgarg260@gmail.com>
2019-11-09 22:24:41 +0530

    Added user model

--- modals/User.js
+++ modals/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = mongoose.Schema({
+const UserSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -22,14 +24,62 @@ const User = mongoose.Schema({
   },
   isactive: {
     type: Boolean,
-    required: true
+    default: false
   },
-  messcard: [
-    [
-      {
-        type: Boolean,
-        default: false
+  messcard: Array,
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
       }
-    ]
+    }
   ]
 });
+
+// Function for findig user in DB with username password
+UserSchema.statics.findByCredentials = async (username, password) => {
+  const user = find({ username });
+  if (!user) {
+    throw Error({ error: 1 });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw Error({ error: 2 });
+  }
+  return user;
+};
+
+//generating auth token for user
+UserSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "messcard");
+
+  //this is for authentication on on device at a time: the previous token will be in valid if user loggs in from a new device each time.
+  user.tokens = [];
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+//overwriting toJson function for for removing password before send it over to client.
+UserSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
+UserSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
